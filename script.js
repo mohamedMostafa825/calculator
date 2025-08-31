c85973e7ecdb0ab64759eb58cab2a3c17ea84ad4
mohamed mostafa <220081834+mohamedMostafa825@users.noreply.github.com>
2025-08-31 16:24:08 +0300

    Create basic calculator functions
    - create functions for addition, subtraction, multiplication, and division
    - create operate function to call suitable function for each operation

---
+++ script.js
@@ -0,0 +1,34 @@
+let operand1, operator, operand2;
+
+function operate(num1, operator, num2) {
+  switch (operator) {
+    case "+":
+      return add(num1, num2);
+      break;
+    case "-":
+      return subtract(num1, num2);
+      break;
+    case "*":
+      return multiply(num1, num2);
+      break;
+    case "/":
+      return divide(num1, num2);
+      break;
+
+    default:
+      break;
+  }
+}
+
+function add(a, b) {
+  return a + b;
+}
+function subtract(a, b) {
+  return a - b;
+}
+function multiply(a, b) {
+  return a * b;
+}
+function divide(a, b) {
+  return a / b;
+}
