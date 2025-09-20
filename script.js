cc938440a9044bca51dbdb8f2febfc09bc9a62ff
mohamed mostafa <220081834+mohamedMostafa825@users.noreply.github.com>
2025-09-20 22:44:02 +0300

    Add initialization, operand tracking and update
    - Introduce `currentOperand` state to track active operand (1 or 2).
    - implement `initialize()` function to reset calculator state.
    - implement `updateOperands()` to receive store digits for the correct operand.

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
-const buttons = document.querySelector("btns");
+const buttons = document.querySelector(".btns");
 const displayScreen = document.querySelector(".display");
 let operand1, operator, operand2;
+
+let currentOperand = 1; // 1 for operand1, 2 for operand2
+
+initialize();
+
+function initialize() {
+  operand1 = 0;
+  operator = "";
+  operand2 = 0;
+  currentOperand = 1; // Reset to first operand
+  display(0);
+}
 
 function operate(num1, operator, num2) {
   switch (operator) {
@@ -38,17 +50,27 @@ function display(num) {
   displayScreen.textContent = num;
 }
 
+function updateOperands(digit) {
+  if (currentOperand == 1) {
+    operand1 = operand1 * 10 + digit;
+  } else if (currentOperand == 2) {
+    operand2 = operand2 * 10 + digit;
+  }
+}
+
 buttons.addEventListener("click", (event) => {
   let btn = event.target;
   switch (btn) {
     case "=":
-      showResult();
+      display(operate(operand1, operator, operand2)); // new
       break;
     case "clear":
       initialize();
       break;
     case "operator":
-      updateOperator(btn.textContent);
+      operator = btn.textContent;
+      operand1 = displayScreen.textContent;
+      currentOperand = 2;
       break;
     default:
       updateOperands(btn.textContent);
